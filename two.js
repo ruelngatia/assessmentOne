59dc7fa28da24eeb6dbc06b077b5369a4a9e3814
ruelngatia <ruelngatia@gmail.com>
2022-10-31 14:55:27 +0300

    rectifications

--- two.js
+++ two.js
@@ -14,3 +14,24 @@
      console.log(result);
 
 
+function rev(s, n){
+
+    let holder = []
+    let temp = []
+
+    for (let index = 0; index < s.length; index++) {
+        temp.push(s[index])
+        if((index+1)%n == 0){
+            holder.push(temp.reverse().join(''))
+            temp = []
+        }else if(index == s.length){
+            holder.push(temp.reverse().join('')) 
+        }
+        
+    }
+
+    holder.join('')
+    console.log(holder.join(''));
+
+}
+rev("Lorem at",4)
